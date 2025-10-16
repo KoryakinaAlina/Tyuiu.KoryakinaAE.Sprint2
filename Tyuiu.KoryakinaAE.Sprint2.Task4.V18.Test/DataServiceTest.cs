7b7c8b02ddf8ff3c34e4db24376cd3a32c6ac2a5
KoryakinaAlina <Tyuiu.KoryakinaAE@outlook.com>
2025-10-16 10:16:55 +0500

    Task4

--- Tyuiu.KoryakinaAE.Sprint2.Task4.V18.Test/DataServiceTest.cs
+++ Tyuiu.KoryakinaAE.Sprint2.Task4.V18.Test/DataServiceTest.cs
@@ -1,6 +1,6 @@
 ﻿using Tyuiu.KoryakinaAE.Sprint2.Task4.V18.Lib;
 
-namespace Tyuiu.Koryakina.AE.Sprint2.Task4.V18.Test
+namespace Tyuiu.KoryakinaAE.Sprint2.Task4.V18.Test
 {
     [TestClass]
     public sealed class DataServiceTest
@@ -10,8 +10,7 @@ namespace Tyuiu.Koryakina.AE.Sprint2.Task4.V18.Test
         {
             DataService ds = new DataService();
             var res = ds.Calculate(1, 0);
-            Assert.AreEqual(0, res); 
-             
-        } 
+            Assert.AreEqual(0, res);
+        }
     }
 }
