e77a80aca9c58e43e487801de661528c4de3525f
KoryakinaAlina <Tyuiu.KoryakinaAE@outlook.com>
2025-10-17 10:18:24 +0500

    Task6

---
+++ Tyuiu.KoryakinaAE.Sprint2.Task6.V11.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.KoryakinaAE.Sprint2.Task6.V11.Lib;
+
+namespace Tyuiu.KoryakinaAE.Sprint2.Task6.V11.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int n = 10;
+            int m = 1;
+            int g = 2007;
+            string wait = "11.01.2007";
+            var res = ds.FindDateOfNextDay(g, m, n);
+            Assert.AreEqual(res, wait);
+        }
+    }
+    
+}
---
+++ Tyuiu.KoryakinaAE.Sprint2.Task6.V11/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.KoryakinaAE.Sprint2.Task6.V11.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #2 | Выполнила: Корякина А.Э | ИСТНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема: Получение результата из switch                                     *");
+        Console.WriteLine("* Задание #6                                                              *");
+        Console.WriteLine("* Вариант #11                                                             *");
+        Console.WriteLine("* Выполнила: Корякина Алина Эльдаяговна | ИСТНб-25-1                  *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("*Дата некоторого дня характеризуется тремя натуральными числами: g (год), *");
+        Console.WriteLine("*m (порядковый номер месяца) и n (число). По заданным g, n и m определить *");
+        Console.WriteLine("*дату следующего дня                                                      *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+        Console.WriteLine("***************************************************************************");
+        int g, m, d;
+        Console.WriteLine("Введите g");
+        g = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите m");
+        m = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите d");
+        d = Convert.ToInt32(Console.ReadLine());
+        var res = ds.FindDateOfNextDay(g, m, d);
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(res);
+        Console.ReadKey();
+    }
+}
